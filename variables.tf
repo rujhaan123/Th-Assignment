90051808c47607952cbc5869b9979cc88e1f6b0e
Rujhaan Bhatia <33410430+rujhaan123@users.noreply.github.com>
2024-04-04 12:35:36 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,24 @@
+variable "GOOGLE_CREDENTIALS" {
+  description = "The Google Cloud credentials"
+  type        = string
+}
+
+variable "project" {
+  description = "Project where we are actually deploying resources"
+  type        = string
+}
+
+variable "region" {
+  description = "The region where resources should be created"
+  type        = string
+}
+
+variable "user" {
+  description = "User"
+  type        = string
+}
+
+
+variable "public_key" {
+  type    = string
+}
